00c629d993a068ff150beb6cfd1bffe407bfef90
Mohamad Malek Alkhodary <malekkhodary@hotmail.com>
2025-02-17 02:23:27 +0300

    initialize project complete Landing page Add utility functions and UI components for improved styling and layout

---
+++ src/components/landing/Footer.tsx
@@ -0,0 +1,110 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+
+const sections = [
+  {
+    title: "Product",
+    links: [
+      { name: "Overview", href: "#" },
+      { name: "Pricing", href: "#" },
+      { name: "Marketplace", href: "#" },
+      { name: "Features", href: "#" },
+    ],
+  },
+  {
+    title: "Company",
+    links: [
+      { name: "About", href: "#" },
+      { name: "Team", href: "#" },
+      { name: "Blog", href: "#" },
+      { name: "Careers", href: "#" },
+    ],
+  },
+  {
+    title: "Resources",
+    links: [
+      { name: "Help", href: "#" },
+      { name: "Sales", href: "#" },
+      { name: "Advertise", href: "#" },
+      { name: "Privacy", href: "#" },
+    ],
+  },
+];
+
+const Footer = () => {
+  return (
+    <section className="py-32">
+      <div className="container m-auto">
+        <footer>
+          <div className="flex flex-col items-center justify-between gap-10 text-center lg:flex-row lg:text-left">
+            <div className="flex w-full max-w-96 shrink flex-col items-center justify-between gap-6 lg:items-start">
+              <div>
+                <span className="flex items-center justify-center gap-4 lg:justify-start">
+                  <Image
+                    src="/freelogo.png"
+                    alt="logo"
+                    width={44}
+                    height={44}
+                  />
+                  <p className="text-3xl font-semibold">codecret</p>
+                </span>
+                <p className="mt-6 text-sm text-muted-foreground">
+                  A good responsive template for your startup business or side
+                  project.
+                </p>
+              </div>
+              <ul className="flex items-center space-x-6 text-muted-foreground">
+                <li className="font-medium hover:text-primary">
+                  <Link href="#">
+                    <FaInstagram className="size-6" />
+                  </Link>
+                </li>
+                <li className="font-medium hover:text-primary">
+                  <Link href="#">
+                    <FaGithub className="size-6" />
+                  </Link>
+                </li>
+                <li className="font-medium hover:text-primary">
+                  <Link href="#">
+                    <FaLinkedin className="size-6" />
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="grid grid-cols-3 gap-6 lg:gap-20">
+              {sections.map((section, sectionIdx) => (
+                <div key={sectionIdx}>
+                  <h3 className="mb-6 font-bold">{section.title}</h3>
+                  <ul className="space-y-4 text-sm text-muted-foreground">
+                    {section.links.map((link, linkIdx) => (
+                      <li
+                        key={linkIdx}
+                        className="font-medium hover:text-primary"
+                      >
+                        <Link href={link.href}>{link.name}</Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="mt-20 flex flex-col justify-between gap-4 border-t pt-8 text-center text-sm font-medium text-muted-foreground lg:flex-row lg:items-center lg:text-left">
+            <p>© 2024 codecret. All rights reserved.</p>
+            <ul className="flex justify-center gap-4 lg:justify-start">
+              <li className="hover:text-primary">
+                <Link href="#"> Terms and Conditions</Link>
+              </li>
+              <li className="hover:text-primary">
+                <Link href="#"> Privacy Policy</Link>
+              </li>
+            </ul>
+          </div>
+        </footer>
+      </div>
+    </section>
+  );
+};
+
+export { Footer };
---
+++ src/components/landing/Hero.tsx
@@ -0,0 +1,115 @@
+import { ExternalLink } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+
+import { Badge } from "@/components/ui/badge";
+import { Button, buttonVariants } from "@/components/ui/button";
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <section className="relative overflow-hidden py-32">
+      <div className="container m-auto">
+        <div className="magicpattern absolute inset-x-0 top-0 -z-10 flex h-full w-full items-center justify-center opacity-100" />
+        <div className="mx-auto flex max-w-5xl flex-col items-center">
+          <div className="z-10 flex flex-col items-center gap-6 text-center">
+            <Image
+              src="/freelogo.png"
+              alt="logo"
+              className="h-16"
+              width={64}
+              height={64}
+            />
+            <Badge variant="outline">UI Blocks</Badge>
+            <div>
+              <h1 className="mb-6 text-pretty text-2xl font-bold lg:text-5xl">
+                Build your next project with Blocks
+              </h1>
+              <p className="text-muted-foreground lg:text-xl">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Elig
+                doloremque mollitia fugiat omnis! Porro facilis quo animi
+                consequatur. Explicabo.
+              </p>
+            </div>
+            <div className="mt-4 flex justify-center gap-2">
+              <Button>Get Started</Button>
+              <Button variant="outline">
+                Learn more <ExternalLink className="ml-2 h-4" />
+              </Button>
+            </div>
+            <div className="mt-20 flex flex-col items-center gap-4">
+              <p className="text-center: text-muted-foreground lg:text-left">
+                Built with open-source technologies
+              </p>
+              <div className="flex flex-wrap items-center justify-center gap-4">
+                <a
+                  href="#"
+                  className={cn(
+                    buttonVariants({ variant: "outline" }),
+                    "group px-3"
+                  )}
+                >
+                  <Image
+                    src="https://shadcnblocks.com/images/block/logos/shadcn-ui-small.svg"
+                    alt="company logo"
+                    className="h-6 saturate-0 transition-all group-hover:saturate-100"
+                    width={24}
+                    height={24}
+                  />
+                </a>
+                <a
+                  href="#"
+                  className={cn(
+                    buttonVariants({ variant: "outline" }),
+                    "group px-3"
+                  )}
+                >
+                  <Image
+                    src="https://shadcnblocks.com/images/block/logos/typescript-small.svg"
+                    alt="company logo"
+                    className="h-6 saturate-0 transition-all group-hover:saturate-100"
+                    width={24}
+                    height={24}
+                  />
+                </a>
+
+                <a
+                  href="#"
+                  className={cn(
+                    buttonVariants({ variant: "outline" }),
+                    "group px-3"
+                  )}
+                >
+                  <Image
+                    src="https://shadcnblocks.com/images/block/logos/react-icon.svg"
+                    alt="company logo"
+                    className="h-6 saturate-0 transition-all group-hover:saturate-100"
+                    width={24}
+                    height={24}
+                  />
+                </a>
+                <a
+                  href="#"
+                  className={cn(
+                    buttonVariants({ variant: "outline" }),
+                    "group px-3"
+                  )}
+                >
+                  <Image
+                    src="https://shadcnblocks.com/images/block/logos/tailwind-small.svg"
+                    alt="company logo"
+                    className="h-4 saturate-0 transition-all group-hover:saturate-100"
+                    width={16}
+                    height={16}
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export { Hero };
