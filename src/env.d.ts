2048779b2408fc494667775118cc55f0f86c792b
codecret <mohammedmalek2016@gmail.com>
2025-08-13 09:21:06 +0400

    refactor(env): comment out RESEND_API_KEY references and update example values in .env.example

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,6 @@ declare namespace NodeJS {
     BETTER_AUTH_EMAIL: string;
     BETTER_AUTH_SECRET: string;
     NEXT_PUBLIC_BETTER_AUTH_URL: string;
-    RESEND_API_KEY: string;
+    // RESEND_API_KEY: string;
   }
 }
--- src/lib/env.ts
+++ src/lib/env.ts
@@ -9,7 +9,7 @@ export const env = createEnv({
       .optional()
       .default("Acme <onboarding@resend.dev>"),
     BETTER_AUTH_SECRET: z.string().min(1),
-    RESEND_API_KEY: z.string().min(1),
+    // RESEND_API_KEY: z.string().min(1),
   },
   shared: {
     NEXT_PUBLIC_BETTER_AUTH_URL: z.string(),
@@ -18,7 +18,7 @@ export const env = createEnv({
     DATABASE_URL: process.env.DATABASE_URL,
     BETTER_AUTH_EMAIL: process.env.BETTER_AUTH_EMAIL,
     BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
-    RESEND_API_KEY: process.env.RESEND_API_KEY,
+    // RESEND_API_KEY: process.env.RESEND_API_KEY,
     NEXT_PUBLIC_BETTER_AUTH_URL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
   },
 });
